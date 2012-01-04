5b01c74c29b68630c220dd7e554504a8b529b1e1
Erik Bray <embray@stsci.edu>
2012-01-04 15:19:53 -0500

    An initial setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='TracEasyOrder',
+    version='0.1',
+    packages='traceasyorder',
+    author='Erik Bray',
+    author_email='erik.m.bray@gmail.com',
+    description='Adds a nicer (JavaScript-based) UI for ordering enumerable '
+                'ticket fields (severity, type, etc.) in Trac.',
+    install_requires=['Trac>=0.12'],
+    entry_points={'trac.plugins': ['traceasyorder = traceasyorder']}
+)
