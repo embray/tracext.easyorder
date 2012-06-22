41e89c99352efa93eae74581fe2cc66b8ef55be3
Erik Bray <embray@stsci.edu>
2012-06-22 09:22:06 -0400

    Update the setup.py to include the js files in installation.

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ from setuptools import setup
 setup(
     name='TracEasyOrder',
     version='0.1',
-    packages=['traceasyorder'],
     author='Erik Bray',
     author_email='erik.m.bray@gmail.com',
     description='Adds a nicer (JavaScript-based) UI for ordering enumerable '
                 'ticket fields (severity, type, etc.) in Trac.',
     install_requires=['Trac>=0.12'],
+    packages=['traceasyorder'],
+    package_data={'traceasyorder': ['htdocs/js/*.js']},
     entry_points={'trac.plugins': ['traceasyorder = traceasyorder']}
 )
